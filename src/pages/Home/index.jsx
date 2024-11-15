25e6bb283dc53b391568feff5b5647963a510ff0
Tấn Dự <tandu542003@gmail.com>
2024-11-15 14:10:29 +0700

    add policy home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -33,11 +33,54 @@ const slideImages = [
 ];
 
 const Home = () => {
+  const policies = [
+    {
+      id: 1,
+      image:
+        "https://res.cloudinary.com/dflpparty/image/upload/v1731371113/LotusDelivery/home_policy_item_image_1_ual2ke.webp",
+      title: "Đặt hàng",
+      content: "Nhanh và dễ dàng",
+    },
+    {
+      id: 2,
+      image:
+        "https://res.cloudinary.com/dflpparty/image/upload/v1731371116/LotusDelivery/home_policy_item_image_2_wbzhh6.webp",
+      title: "Từ hệ thống",
+      content: "Các nhà hàng Chuẩn Nhật",
+    },
+    {
+      id: 3,
+      image:
+        "https://res.cloudinary.com/dflpparty/image/upload/v1731371119/LotusDelivery/home_policy_item_image_3_ai0tjx.webp",
+      title: "Giao hàng",
+      content: "An toàn & Đảm Bảo",
+    },
+    {
+      id: 4,
+      image:
+        "https://res.cloudinary.com/dflpparty/image/upload/v1731371121/LotusDelivery/home_policy_item_image_4_xbuiqp.webp",
+      title: "Hàng ngàn",
+      content: "Ưu đãi hấp dẫn nhất",
+    },
+  ];
+
   return (
     <>
       <div className="home">
         <SlideShow slideImages={slideImages} />
-        <div className="container"></div>
+        <div className="container">
+          <div className="home-policy">
+            {policies.map((policy, index) => (
+              <div key={index} className="home-policy-item">
+                <img src={policy.image} alt={policy.title} />
+                <div className="policy-content">
+                  <span>{policy.title}</span>
+                  <b>{policy.content}</b>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
